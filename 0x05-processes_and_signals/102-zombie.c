f1440e7e60e3d9997546a42760ac5da03901a22d
userfiona <fionaramaesela@gmail.com>
2023-07-28 13:43:13 -0700

    changed

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -6,46 +6,24 @@
 /**
  * main - zombies
  *
- * Description: create five zombies
+ * Description: make five zombies
  * Return: 0 for success
  */
 int main(void)
 {
-    int i;
-    pid_t pidme;
+	int i;
+	pid_t pidme;
 
-    for (i = 0; i < 5; i++)
-    {
-        pidme = fork();
-
-        if (pidme < 0)
-        {
-            perror("fork");
-            exit(EXIT_FAILURE);
-        }
-        else if (pidme == 0)
-        {
-            // Child process
-            exit(0);
-        }
-        else
-        {
-            // Parent process
-            printf("Zombie process created, PID: %i\n", pidme);
-        }
-    }
-
-    // The parent process sleeps for a while to allow the zombies to remain
-    // in the system before being reaped.
-    sleep(100);
-
-    // The parent process should now reap the zombies.
-    for (i = 0; i < 5; i++)
-    {
-        int status;
-        pid_t zombie_pid = wait(&status);
-        printf("Zombie process %i reaped, exit status: %d\n", zombie_pid, WEXITSTATUS(status));
-    }
-
-    return (0);
+	i = 0;
+	while (i < 5)
+	{
+		pidme = fork();
+		if (pidme)
+			printf("Zombie process created, PID: %i\n", pidme);
+		else
+			exit(0);
+		i++;
+	}
+	sleep(100);
+	return (0);
 }
