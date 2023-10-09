e8e84106cc5ab73704eae3fce3688652ea0432ca
userfiona <fionaramaesela@gmail.com>
2023-10-08 18:07:03 -0700

    grep

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -1,17 +1,8 @@
-exec {'update':
-  command => '/usr/bin/apt-get update',
-}
-
-package {'nginx':
-  ensure => 'present',
-}
-
-file_line { 'http_header':
-  path  => '/etc/nginx/sites-available/default',
-  match => 'server {',
-  line  => "server {\n\tadd_header X-Served-By \"${hostname}\";\n",
-}
-
-exec {'run':
-  command => '/usr/sbin/service nginx restart',
+# Automation: creates a custom HTTP header response with Puppet.
+exec { 'command':
+  command  => 'apt-get -y update;
+  apt-get -y install nginx;
+  sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
+  service nginx restart',
+  provider => shell,
 }
