51656cb1e24523f73ce930004e576825f4b46964
userfiona <fionaramaesela@gmail.com>
2023-10-18 09:57:16 -0700

    ssh

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,7 +2,7 @@
 file_line { 'Identity file':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
-  line   => '    IdentityFile ~/.ssh/id_rsa',
+  line   => '    IdentityFile ~/.ssh/school',
 }
 file_line { 'disable password login':
     path    => '/etc/ssh/ssh_config',
