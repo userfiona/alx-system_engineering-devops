25d9f2a99c0d4e59c380aae2e001d448b7b0f3f4
userfiona <fionaramaesela.com>
2023-10-24 18:34:29 -0700

    api

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,29 +1,26 @@
 #!/usr/bin/python3
-"""Exports data in the JSON format"""
+"""
+    Extend Python script in 0-gather_data_from_an_API.py file to export data
+    in the JSON format.
+"""
+
 
 if __name__ == "__main__":
-
     import json
     import requests
-    import sys
+    from sys import argv
 
-    userId = sys.argv[1]
-    user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
-                        .format(userId))
-    todos = requests.get('https://jsonplaceholder.typicode.com/todos')
-    todos = todos.json()
+    url = "https://jsonplaceholder.typicode.com"
+    userId = argv[1]
+    employee = requests.get(
+            "{}/users/{}".format(url, userId)).json().get("username")
+    tasks = requests.get("{}/todos?userId={}".format(url, userId)).json()
+    my_list = [{
+        "task": dict.get("title"),
+        "completed": dict.get("completed"),
+        "username": employee}
+        for dict in tasks]
 
-    todoUser = {}
-    taskList = []
-
-    for task in todos:
-        if task.get('userId') == int(userId):
-            taskDict = {"task": task.get('title'),
-                        "completed": task.get('completed'),
-                        "username": user.json().get('username')}
-            taskList.append(taskDict)
-    todoUser[userId] = taskList
-
-    filename = userId + '.json'
-    with open(filename, mode='w') as f:
-        json.dump(todoUser, f)
+    filename = "{}.json".format(userId)
+    with open(filename, 'w') as json_file:
+        json.dump({userId: my_list}, json_file)
