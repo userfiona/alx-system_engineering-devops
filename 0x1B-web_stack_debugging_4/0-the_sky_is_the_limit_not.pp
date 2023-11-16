409faa5882447fb0bff770ea6880fff8688aba66
userfiona <fionaramaesela@gmail.com>
2023-11-16 17:55:12 +0000

    de

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,3 +1,11 @@
-# Puppet script
-exec { '/usr/bin/env sed -i s/15/1000/ /etc/default/nginx': }
--> exec { '/usr/bin/env service nginx restart': }
+# this script Increase open file limit
+exec {'increase traffic':
+  onlyif  => 'test -e /etc/default/nginx',
+  command => 'sed -i "5s/[0-9]\+/$( ulimit -n )/" /etc/default/nginx',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
+# Restart Nginx
+exec { 'nginx restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
+}
